c90d4d266ba65900a09bf32369f70d3554925596
PranaavPrasad <pranaav.prasad@gmail.com>
2024-04-05 16:24:32 +0530

    pranaav create components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+import Reacht
+import EventCard from "./EventCard";
---
+++ client/src/components/EventCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function EventCard(props) {
+  return (
+    <div >
+      <div >
+        <h2>{props.name}</h2>
+        <img src={props.img} alt="avatar_img" />
+      </div>
+      <div>
+        <p>{props.date}</p>
+        <p>{props.time}</p>
+        <p>{props.venue}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
