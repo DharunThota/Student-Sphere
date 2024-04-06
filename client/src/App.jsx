b34b0335ff17e2be4cb65869bbc24d5bdfe672c5
Pratyek Thumula <pratyekpk3@gmail.com>
2024-04-06 14:53:12 +0530

    p1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
+import Events from './routes/Events';
 // Other imports
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        
+        <Route path='/Events' element={<Events/>} />
       </Routes>
     </Router>
   );
