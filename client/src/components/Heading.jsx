f2ddbe2476bd3e6dcf2818c77f5edcbf12ec628a
222rohan <rohanshenoy405@gmail.com>
2024-04-07 17:44:15 +0530

    r added heading and subheading component for entire site

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+import './Heading.css';
+
+function Heading(props){
+    return (
+        <div className="heading">
+            <h1>{props.name}</h1>
+        </div>
+    );
+}
+export default Heading;
