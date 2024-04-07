f91a8246a02cb4d9c444c06cda4e289feb0a84e8
222rohan <rohanshenoy405@gmail.com>
2024-04-07 17:43:38 +0530

    r changed structure of card

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -5,12 +5,12 @@ function EventCard(props) {
     <div className="ecard">
       <div>
         <img src={props.img} alt="avatar_img" />
-        <h1>{props.name}</h1>
+        <h5>{props.name}</h5>
       </div>
-      <div className="body">        
-        <p>{props.date}</p>
-        <p>{props.time}</p>
-        <p>{props.venue}</p>        
+      <hr />
+      <div className="body">     
+        <p>{props.date}, {props.time}</p>
+        <p>{props.venue}</p>    
       </div>
     </div>
   );
