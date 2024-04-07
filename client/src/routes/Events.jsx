c84e3ef359ff8079b39f2a4d64dec01c07da4dd9
222rohan <rohanshenoy405@gmail.com>
2024-04-07 15:04:09 +0530

    r added needed files for eventspage

--- client/src/routes/Events.jsx
+++ client/src/routes/Events.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import './Events.css';
 import EventCard from "../components/EventCard";
 import Eventlist from "../components/Eventlist";
+import ButtonGroup from "../components/ButtonGroup";
 
 function Events(){
-    return(
+    return(//heading
+    //subheading  -- buttons right side
+    //event cards
+
         <div className="container">
+        <ButtonGroup/>
+
             {Eventlist.map((event) => (
                 <EventCard
                     key={event.id}
