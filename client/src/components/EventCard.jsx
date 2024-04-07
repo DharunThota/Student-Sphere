363643324da6e27b676d9999e8c39cf84cc04149
PranaavPrasad <pranaav.prasad@gmail.com>
2024-04-07 21:27:59 +0530

    pp

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -7,19 +7,10 @@ function EventCard(props) {
         <img src={props.img} alt="avatar_img" />
         <h5>{props.name}</h5>
       </div>
-<<<<<<< HEAD
-      <div className="body">        
-        <p>{props.date}</p>
-        <break />
-        <p>{props.time}</p>
-        <break />
-        <p>{props.venue}</p>        
-=======
       <hr />
       <div className="body">     
         <p>{props.date}, {props.time}</p>
         <p>{props.venue}</p>    
->>>>>>> 13d6c505742c92b5a8497174cb91734ae4a71080
       </div>
       <div className="info">
         <p>{props.des}</p>
