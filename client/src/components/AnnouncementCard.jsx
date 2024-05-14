46c3f99577b075e74e2d6bfa3e4af392998d71a1
PranaavPrasad <pranaav.prasad@gmail.com>
2024-05-14 11:55:11 +0530

    pp

--- client/src/components/AnnouncementCard.jsx
+++ client/src/components/AnnouncementCard.jsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Announcements.css";
-import popup from "./AnnouncementPopup";
+import { Modal, Button } from "react-bootstrap";
 
 function AnnouncementCard(props) {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);  
+
   return (
-    <div className="container" onClick={popup}>
-      <div className="acard">
+    <>
+    <div className="container" >
+      <div className="acard" onClick={handleShow}>
         <p>{props.heading}</p>
-        <h>{props.club}</h>
+        <h5>{props.club}</h5>
       </div>
+
+      <Modal 
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{props.heading}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {props.desc}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
     </div>
+    </>
   );
+
 }
 
 export default AnnouncementCard;
