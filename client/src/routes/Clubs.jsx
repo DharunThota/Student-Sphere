61ce5bfe19ea8fd757c2ccdf9f0ad96f0392cf64
Dharun Thota <dharunth@gmail.com>
2024-05-16 21:54:31 +0530

    d add search

--- client/src/routes/Clubs.jsx
+++ client/src/routes/Clubs.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import '../styles/Clubs.css';
 import ClubCard from "../components/ClubCard";
@@ -9,25 +9,78 @@ import Navbar from "../components/Navbar";
 function Clubs(){
     const [tech, setTech] = useState([]);
     const [cult, setCult] = useState([]);
+    const [searchActive, setSearchActive] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const inputRef = useRef(null);
+    const [temp1, setTemp1] = useState([]);
+    const [temp2, setTemp2] = useState([]);
 
     useEffect(() => {
         async function fetchData(){
             const res1 = await axios.get("http://localhost:3000/clubs/Technical");
-            //console.log(res1.data);
             setTech(res1.data)
+            setTemp1(res1.data);
             const res2 = await axios.get("http://localhost:3000/clubs/Cultural");
-            //console.log(res2.data);
             setCult(res2.data)
+            setTemp2(res2.data);
         }
         fetchData();
     }, [])
+
+    useEffect(() => {
+        if(searchTerm === '') {
+            setTech(temp1);
+            setCult(temp2);
+            return;
+        }
+        const filterTech = temp1.filter(club =>
+            club.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+        );
+        setTech(filterTech);
+
+        const filterCult = temp2.filter(club =>
+            club.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+        );
+        setCult(filterCult);
+    }, [searchTerm]);
+
+    const handleSearchClick = () => {
+        setSearchActive(!searchActive);
+        if (!searchActive) {
+            inputRef.current.focus();
+        }
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
 
     return(
         <>
             <Navbar />
             <div className="club-container">
                 <div className="clubCard">
-                    <Heading name="Clubs" />
+                <div className="Header">
+                    <div><Heading name="Clubs" /></div>
+                    <div className={`search-container ${searchActive ? 'active' : ''}`}>
+                        <div className={`search-icon ${searchActive ? 'd-none' : ''}`} onClick={handleSearchClick}>
+                            <i className="fa fa-search"></i>
+                        </div>
+                        <input
+                            ref={inputRef}
+                            type="text"
+                            className="search-input"
+                            placeholder="Search..."
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            onBlur={handleSearchClick}
+                        />
+                    </div>
+                </div>
+
+                <hr className='hori'/>
+
+
                     <SubHeading name="Technical Clubs" />
                     <div className="container">
                         {tech.map((club) => (
--- client/src/routes/Events.jsx
+++ client/src/routes/Events.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import '../styles/Events.css';
 import EventCard from "../components/EventCard";
-import ButtonGroup from "../components/ButtonGroup";
 import Heading from '../components/Heading'
 import SubHeading from "../components/SubHeading";
 import Navbar from "../components/Navbar";
@@ -11,16 +10,26 @@ function Events(){
     const [upcoming, setUpcoming] = useState([]);
     const [ongoing, setOngoing] = useState([]);
     const [completed, setCompleted] = useState([]);
+    const [searchActive, setSearchActive] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const inputRef = useRef(null);
+    const [temp1, setTemp1] = useState([]);
+    const [temp2, setTemp2] = useState([]);
+    const [temp3, setTemp3] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const r1 = await axios.get("http://localhost:3000/events/Upcoming");
                 setUpcoming(r1.data);
+                setTemp1(r1.data);
                 const r2 = await axios.get("http://localhost:3000/events/Ongoing");
                 setOngoing(r2.data);
+                setTemp2(r2.data);
                 const r3 = await axios.get("http://localhost:3000/events/Completed");
                 setCompleted(r3.data);
+                setTemp3(r3.data);
+                
             } catch (error) {
                 console.error("Error fetching data: ", error);
             }
@@ -28,17 +37,68 @@ function Events(){
         fetchData();
     }, []);
 
+    useEffect(() => {
+        if(searchTerm === '') {
+            setUpcoming(temp1);
+            setOngoing(temp2);
+            setCompleted(temp3);
+            return;
+        }
+        const filterUpcoming = temp1.filter(event =>
+            event.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+        );
+        setUpcoming(filterUpcoming);
+
+        const filterOngoing = temp2.filter(event =>
+            event.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+        );
+        setOngoing(filterOngoing);
+
+        const filterCompleted = temp3.filter(event =>
+            event.title.toLowerCase().startsWith(searchTerm.toLowerCase())
+        );
+        setCompleted(filterCompleted);
+    }, [searchTerm]);
+
+    const handleSearchClick = () => {
+        setSearchActive(!searchActive);
+        if (!searchActive) {
+            inputRef.current.focus();
+        }
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
 
     return(
         <>
             <Navbar />
-            <div className="club-container">
-            <Heading name="Events" />
-            <SubHeading name="Upcoming Events" />
+            <div className='club-container'>
+                <div className="Header">
+                    <div><Heading name="Events" /></div>
+                    <div className={`search-container ${searchActive ? 'active' : ''}`}>
+                        <div className={`search-icon ${searchActive ? 'd-none' : ''}`} onClick={handleSearchClick}>
+                            <i className="fa fa-search"></i>
+                        </div>
+                        <input
+                            ref={inputRef}
+                            type="text"
+                            className="search-input"
+                            placeholder="Search..."
+                            value={searchTerm}
+                            onChange={handleSearchChange}
+                            onBlur={handleSearchClick}
+                        />
+                    </div>
+                </div>
+
+                <hr className='hori'/>
+                <SubHeading name="Upcoming Events" />
                 <div className="container-fluid">
                     {upcoming.map((event) => (
                         <EventCard
-                            id = {event.event_id}
+                            id={event.event_id}
                             key={event.event_id}
                             name={event.name}
                             date={event.date}
@@ -48,7 +108,6 @@ function Events(){
                         />
                     ))}
                 </div>
-
                 <SubHeading name="Ongoing Events" />
                 <div className="container-fluid">
                     {ongoing.map((event) => (
@@ -78,7 +137,6 @@ function Events(){
                         />
                     ))}
                 </div>
-
             </div>
         </>
     );
